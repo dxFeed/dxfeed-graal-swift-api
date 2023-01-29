03d15b84e20b1c0fd5c3ea1e8e8f8f67b9593c2e
Aleksey Kosylo <alekskosylo@gmail.com>
2023-01-29 17:40:12 +0400

    add test files

---
+++ DXFeedFramework/TestGraalvm.h
@@ -0,0 +1,16 @@
+//
+//  TestGraalvm.h
+//  DXFeedFramework
+//
+//  Created by Aleksey Kosylo on 1/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestGraalvm : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DXFeedFramework/TestGraalvm.m
@@ -0,0 +1,12 @@
+//
+//  TestGraalvm.m
+//  DXFeedFramework
+//
+//  Created by Aleksey Kosylo on 1/29/23.
+//
+
+#import "TestGraalvm.h"
+
+@implementation TestGraalvm
+
+@end
