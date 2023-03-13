c9d3de9a787289e0660c2e9aec444a60425faa83
Aleksey Kosylo <alekskosylo@gmail.com>
2023-03-13 22:38:00 +0400

    Initial Commit

---
+++ DxFeedSwiftFramework/DxFeedSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  DxFeedSwiftFramework.h
+//  DxFeedSwiftFramework
+//
+//  Created by Aleksey Kosylo on 13.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DxFeedSwiftFramework.
+FOUNDATION_EXPORT double DxFeedSwiftFrameworkVersionNumber;
+
+//! Project version string for DxFeedSwiftFramework.
+FOUNDATION_EXPORT const unsigned char DxFeedSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DxFeedSwiftFramework/PublicHeader.h>
+
+
