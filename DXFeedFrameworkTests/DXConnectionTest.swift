f0dca44e93598071642c54a907a675c486309573
AKosylo <alekskosylo@gmail.com>
2023-12-14 17:07:33 +0100

    increase retry attempts for test small changes(remove call gc in tests)

--- DXFeedFrameworkTests/DXConnectionTest.swift
+++ DXFeedFrameworkTests/DXConnectionTest.swift
@@ -72,11 +72,4 @@ final class DXConnectionTest: XCTestCase {
 
         wait(for: [receivedEventsExpectation], timeout: 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
