1b9b58ef59e82d0f822d4372da9c019fe465b8ba
Aleksey Kosylo <akosylo@dxfeed.com>
2024-06-19 09:58:59 +0200

    add samples: quote table(based on quotes and profiles) perf test: calculate speed events/sec

---
+++ DxFeedPertTestApp/QuoteCell.swift
@@ -0,0 +1,58 @@
+//
+//  QuoteCell.swift
+//  TestApp
+//
+//  Created by Aleksey Kosylo on 07.06.23.
+//
+
+import UIKit
+
+class QuoteCell: UITableViewCell {
+    @IBOutlet var symbolNameLabel: UILabel!
+    @IBOutlet var askLabel: UILabel!
+    @IBOutlet var bidLabel: UILabel!
+
+    @IBOutlet var priceContentView: UIView!
+    @IBOutlet var backgroundContentView: UIView!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+    let colors = Colors()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundContentView.layer.cornerRadius = 10
+        self.backgroundContentView.clipsToBounds = true
+        self.contentView.backgroundColor = colors.background
+        self.backgroundContentView.backgroundColor = colors.cellBackground
+        priceContentView.layer.cornerRadius = 10
+        priceContentView.clipsToBounds = true
+    }
+
+    func updateAsk(value: Bool?) {
+        updateLabelBackground(value: value, label: askLabel)
+    }
+
+    func updateBid(value: Bool?) {
+        updateLabelBackground(value: value, label: bidLabel)
+    }
+
+    private func updateLabelBackground(value: Bool?, label: UILabel) {
+        guard let value = value else {
+            label.backgroundColor = colors.priceBackground
+            return
+        }
+        label.backgroundColor = value ? colors.green : colors.red
+    }
+
+    func update(model: QuoteModel?, symbol: String, description: String?) {
+        symbolNameLabel.text = (symbol.components(separatedBy: ":").first ?? "") + "\n" + (description ?? "")
+        askLabel.text = model?.ask
+        updateAsk(value: model?.increaseAsk)
+        updateBid(value: model?.increaseBid)
+        bidLabel.text = model?.bid
+
+    }
+}
---
+++ DxFeedPertTestApp/QuoteModel.swift
@@ -0,0 +1,67 @@
+//
+//  QuoteModel.swift
+//  TestApp
+//
+//  Created by Aleksey Kosylo on 07.06.23.
+//
+
+import Foundation
+import DxFeedSwiftFramework
+
+class QuoteModel {
+
+    let formatter = NumberFormatter()
+
+    private var current: Quote?
+    private var previous: Quote?
+    private var descriptionStr: String = ""
+
+    init() {
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 4
+    }
+
+    var ask: String {
+        guard let price = current?.askPrice else {
+            return "---"
+        }
+        let number = NSNumber(value: price)
+        return formatter.string(from: number) ?? ""
+    }
+
+    var bid: String {
+        guard let price = current?.bidPrice else {
+            return "---"
+        }
+        let number = NSNumber(value: price)
+        return formatter.string(from: number) ?? ""
+    }
+
+    var descriptionString: String {
+        return descriptionStr
+    }
+
+    var increaseAsk: Bool? {
+        guard let current = current?.askPrice, let previous = previous?.askPrice else {
+            return nil
+        }
+        return current > previous
+    }
+
+    var increaseBid: Bool? {
+        guard let current = current?.bidPrice, let previous = previous?.bidPrice else {
+            return nil
+        }
+        return current > previous
+    }
+
+    func update(_ value: Quote) {
+        previous = current
+        current = value
+    }
+
+    func update(_ desc: String) {
+        descriptionStr = desc
+    }
+
+}
