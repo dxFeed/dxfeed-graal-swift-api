796c03dec5a0ded135acef3ff7331249915257c8
dxcity <dxcity@bots.devexperts.com>
2026-08-04 19:12:58 +0200

    build 1.6.0

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let version = "1.5.0"
+let version = "1.6.0"
 let moduleName = "DXFeedFramework"
-let checksum = "002bd9096b967096ca994cec3ab8826c368eb1edbaf584928cd5628a2fcd0e84"
+let checksum = "b25d9580d25d985225d8d6a569f3c749ef3de65a22156f432da00b1e1410c1d6"
 
 let package = Package(
     name: moduleName,
